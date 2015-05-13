31ac03d5065e13866c3e1c5922184325d6e71541
Guy John <guy@rumblesan.com>
2015-05-13 16:22:21 +0100

    Only extend web audio classes it the web audio api is available

--- src/globals.js
+++ src/globals.js
@@ -1,36 +1,42 @@
 /*global AudioParam, OscillatorNode, BiquadFilterNode */
 
-AudioParam.prototype.set = function (newValue, audioCtx) {
-    if (this.setValueAtTime) {
-        this.setValueAtTime(newValue, audioCtx.currentTime);
-    } else {
-        this.value = newValue;
-    }
-};
+var root = this;
 
-OscillatorNode.prototype.getWaveParam = function () {
-    var self = this;
-    return {
-        set: function (waveType, audioCtx) {
-            self.type = waveType;
-        },
-        get: function () {
-            return self.type;
+if (root.AudioContext || root.webkitAudioContext) {
+
+    AudioParam.prototype.set = function (newValue, audioCtx) {
+        if (this.setValueAtTime) {
+            this.setValueAtTime(newValue, audioCtx.currentTime);
+        } else {
+            this.value = newValue;
         }
     };
-};
 
-BiquadFilterNode.prototype.getFilterTypeParam = function () {
-    var self = this;
-    return {
-        set: function (filterType, audioCtx) {
-            self.type = filterType;
-        },
-        get: function () {
-            return self.type;
-        }
+    OscillatorNode.prototype.getWaveParam = function () {
+        var self = this;
+        return {
+            set: function (waveType, audioCtx) {
+                self.type = waveType;
+            },
+            get: function () {
+                return self.type;
+            }
+        };
     };
-};
+
+    BiquadFilterNode.prototype.getFilterTypeParam = function () {
+        var self = this;
+        return {
+            set: function (filterType, audioCtx) {
+                self.type = filterType;
+            },
+            get: function () {
+                return self.type;
+            }
+        };
+    };
+
+}
 
 module.exports = {
     imported: true
